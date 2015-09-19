e449a472137879cd73b51260836500b8d22195a9
Karl Mutch <karlmutch@gmail.com>
2015-09-19 08:33:57 -0700

    Renamed developer machine outputs to reflect general purpose development rather than Go specific

--- Dev/build.bat
+++ Dev/build.bat
@@ -2,4 +2,4 @@ mkdir input || true
 mkdir input/virtualbox || true
 rm input/virtualbox/*.* input/virtualbox/Vagrantfile || true
 cp -r $(echo $USERPROFILE)/.vagrant.d/boxes/BaseBox/0/virtualbox/ input/.
-packer build GoDev.json
+packer build Dev.json
