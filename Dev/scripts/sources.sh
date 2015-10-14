cd20c0de0efa14040be5cf9a7e7a9c58eff3588a
Karl Mutch <karlmutch@gmail.com>
2015-10-13 22:48:31 -0700

    Added Rust based exa and drop old Ubuntu docker custom repo which is now out of date

--- Dev/scripts/sources.sh
+++ Dev/scripts/sources.sh
@@ -8,5 +8,3 @@ sudo apt-get update -y
 sudo apt-get install -y tmux
 
 sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
-
-sudo add-apt-repository -y 'deb https://get.docker.io/ubuntu docker main'
