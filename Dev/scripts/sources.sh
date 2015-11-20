5669ffeb78122d6dffd55a4f500c77b9b758e122
Karl Mutch <karlmutch@gmail.com>
2015-11-19 20:36:36 -0800

    Sublime and atom upgrades

--- Dev/scripts/sources.sh
+++ Dev/scripts/sources.sh
@@ -5,6 +5,9 @@ sudo add-apt-repository -y ppa:git-core/ppa
 echo "TMUX repository"
 sudo add-apt-repository -y ppa:pi-rho/dev
 sudo add-apt-repository -y ppa:djcj/screenfetch
+sudo add-apt-repository ppa:webupd8team/sublime-text-2
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+
 sudo apt-get update -y
 sudo apt-get install -y tmux
 
