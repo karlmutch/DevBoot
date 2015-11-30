213be068d9fd756a6187863f336b2d59d1b0f8c7
Karl Mutch <karlmutch@gmail.com>
2015-11-29 16:15:30 -0800

    Add NeoVim

--- Dev/scripts/sources.sh
+++ Dev/scripts/sources.sh
@@ -5,6 +5,7 @@ sudo add-apt-repository -y ppa:git-core/ppa
 echo "TMUX repository"
 sudo add-apt-repository -y ppa:pi-rho/dev
 sudo add-apt-repository -y ppa:djcj/screenfetch
+sudo add-apt-repository -y ppa:neovim-ppa/unstable
 sudo add-apt-repository ppa:webupd8team/sublime-text-2
 sudo add-apt-repository ppa:webupd8team/sublime-text-3
 
