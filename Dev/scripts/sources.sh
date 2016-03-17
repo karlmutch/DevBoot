9da522ee2279ec389e2952319ace0cb89010ffeb
Karl Mutch <karlmutch@gmail.com>
2016-03-17 13:17:04 -0700

    Get Rump components working

--- Dev/scripts/sources.sh
+++ Dev/scripts/sources.sh
@@ -15,11 +15,7 @@ sudo add-apt-repository -y ppa:webupd8team/sublime-text-3
 # Terminator repository
 sudo add-apt-repository -y ppa:gnome-terminator
 
-
-# RUMP tools libxen-dev and qemu-kvm
-sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
 
 sudo apt-get update -y
 sudo apt-get install -y tmux
-
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
